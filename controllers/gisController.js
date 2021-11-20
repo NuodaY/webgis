944b8b04482c30c51b072a481fa8d7bf6c5fb309
NuodaY <nuoday@student.unimelb.edu.au>
2021-11-20 16:39:16 +1100

    finished measurement features

--- controllers/gisController.js
+++ controllers/gisController.js
@@ -46,10 +46,23 @@ const displayTyphoonDetails = function(req, res) {
 
 // 显示计算结果
 const displayResults = function(req, res) {
-    var pointA = req.body.pta;
-    var pointB = req.body.ptb;
-    var result = 5;
+    var pointAX = req.body.ptax; 
+    var pointAY = req.body.ptay;
+    var pointBX = req.body.ptbx;
+    var pointBY = req.body.ptby;
+    var result = calculateDistance(pointAX, pointAY, pointBX, pointBY);
     return res.render("results", {"result": result});
+}
+
+const calculateDistance = function(pointAX, pointAY, pointBX, pointBY){
+    var ver = 2*Math.PI*6357/360*Math.abs(pointAY-pointBY);
+    var hor = 2*Math.PI*6378/360*Math.abs(pointAX-pointBX);
+    var coefficient = Math.abs(Math.cos(Math.PI/180*(Number(pointAY)+Number(pointBY))/2));
+    hor *= coefficient;
+    var result = Math.sqrt(ver*ver+hor*hor);
+    console.log("Calculated distance:");
+    console.log(result);
+    return result;
 }
 
 // 导出函数
