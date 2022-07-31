02c65e86a50b25555ff55b757132d3bfedf30456
Nuoda Yang <nuoda.yang@gmail.com>
2022-07-31 11:39:57 +0800

    minor updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars")
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.engine('hbs',exphbs({
     defaultLayout: 'main',
